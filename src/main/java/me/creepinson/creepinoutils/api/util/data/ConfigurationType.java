211f34126c9a185559815ac60e6772fb62aef1fa
Theo Paris <theoparis@pop-os.localdomain>
2020-06-22 20:28:31 -0700

    dded tensors

---
+++ src/main/java/me/creepinson/creepinoutils/api/util/data/ConfigurationType.java
@@ -0,0 +1,7 @@
+package me.creepinson.creepinoutils.api.util.data;
+
+/**
+ * @author Creepinson https://theoparis.com/about
+ **/
+public enum ConfigurationType {
+}
---
+++ src/main/java/me/creepinson/creepinoutils/api/util/math/Tensor.java
@@ -0,0 +1,7 @@
+package me.creepinson.creepinoutils.api.util.math;
+
+/**
+ * @author Creepinson https://theoparis.com/about
+ **/
+public class Tensor {
+}
