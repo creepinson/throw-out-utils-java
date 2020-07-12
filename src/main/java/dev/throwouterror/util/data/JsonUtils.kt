f2e5499d0a11a2ae4beb4081a8b8fd1d88108c08
Theo <theoplaysmc@gmail.com>
2020-07-12 14:56:29 -0700

    added extensions to jsonutils

--- src/main/java/dev/throwouterror/util/data/JsonUtils.kt
+++ src/main/java/dev/throwouterror/util/data/JsonUtils.kt
@@ -7,8 +7,16 @@ import com.google.gson.GsonBuilder
  * @author Creepinson https:/theoparis.com/about
  */
 object JsonUtils {
-    private val gson = GsonBuilder().setPrettyPrinting().serializeNulls().disableHtmlEscaping().create()
+    private val gson = GsonBuilder().setPrettyPrinting().disableHtmlEscaping().create()
     fun get(): Gson {
         return gson
     }
+}
+
+fun Any.toJsonString(): String {
+    return JsonUtils.get().toJson(this)
+}
+
+fun String.fromJsonString(type: Class<*>): Any {
+    return JsonUtils.get().fromJson(this, type)
 }
